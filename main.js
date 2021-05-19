775cf72e22c0502a4b8007a8e820cb6584971f37
Javohir <javohirazizov73@gmail.com>
2021-05-19 20:38:28 +0500

    first JS project

---
+++ main.js
@@ -0,0 +1,48 @@
+ var hi =('Hi 👋🏻');
+ alert(hi);
+
+ var myName = prompt ('Enter your name below 🤖👇🏻')
+
+ 
+ alert( 'Hello👾   '+ myName );
+
+ var myAge = +prompt('How old are you🗿  '+ myName)
+
+ alert(   + 2021 - myAge + '    Is your birth year  😁😁!!!  '  )
+
+ var questions =  ('I have some questions for you 🧩🎲;  OK?')
+
+ confirm (questions)
+
+ var first = prompt('12+6')
+ var second = prompt('5*6')
+ var third = prompt('24/6')
+ var fourth = prompt('13+16')
+ 
+var pause = ('Not tired??  😴😴');
+confirm(pause)
+
+var last = ('Last one!!!  🥳🥳');
+
+alert(last);
+
+var fifth = prompt('9*9+9');
+
+var least = prompt ('what was your first answer 😂😂😂🙁')
+
+
+var wow = ('WOW');
+alert(wow)
+
+var final = ('Now you can see your results in console  👇🏻👇🏻')
+
+confirm(final)
+
+console.log('12+6=18  ' + 'your answer is 👉🏻  ' + first );
+console.log('5*6=30  ' + 'your answer is 👉🏻  ' + second );
+console.log('24/6=4  ' + 'your answer is 👉🏻  ' + third );
+console.log('13+16=29  ' + 'your answer is 👉🏻  ' + fourth );
+console.log('9*9+9=162  ' + 'your answer is 👉🏻  ' + fifth );
+console.log('first one was  ' + least + '👉🏻  ' + least )
+
+ 
